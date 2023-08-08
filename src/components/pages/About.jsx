898239aa332101f45f09211fc057c4b6e1166df7
Nikhil Naik <46309727+nikhilsurfingaus@users.noreply.github.com>
2023-08-08 14:50:38 +1000

    scroll up effect, border fixes on about page and home page slight mobile resoivneve bug fix

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import AboutHero from '../AboutHero';
 import AboutFAQ from '../AboutFAQ';
 import AboutInspire from '../AboutInspire';
+import ScrollToTop from "react-scroll-to-top";
 
 const About = () => {
   return (
     <div>
+      <ScrollToTop smooth color="#1A64BA " />
       <AboutHero />
       <hr className="glowing-hr" /> 
       <AboutFAQ />
